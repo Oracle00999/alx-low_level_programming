37d0db4c911527a9714322824b3806a1f13eb573
Oracle00999 <nwazotachibuike@gmail.com>
2022-09-09 00:47:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -11,5 +10,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0)
 }
