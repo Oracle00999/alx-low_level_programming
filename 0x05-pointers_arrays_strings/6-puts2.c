596f7b1a1a6d855e0ba6ae6e3657f1aef4f15a37
oracle00999 <nwazotachibuike@gmail.com>
2022-09-19 07:01:45 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,12 @@
 void puts2(char *str)
 {
 	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-			i++;
-		}
-	}
-	_putchar('\n');
+i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
